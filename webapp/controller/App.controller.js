3a4c4dd8162864e00ec0ae57623c8bd555398813
Romeo Antonio Azurin <anton.azurin@bnext.tech>
2022-06-16 04:00:03 +0000

    AZURIN: Initial Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("zui3derp.controller.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
