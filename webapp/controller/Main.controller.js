4a83b9e657920b95daca27ac3d23c6c8a0ba6c2f
Romeo Antonio Azurin <anton.azurin@bnext.tech>
2022-06-22 07:08:35 +0000

    Added: Newsfeed and Stat Board at Main Page

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,5 +1,6 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
+    "sap/m/library",
     "sap/ui/model/json/JSONModel"
 ],
     /**
@@ -9,7 +10,48 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("zui3derp.controller.Main", {
-
             
-        });
+        onInit: function(){
+                this.onStyleReader();
+        },
+
+        onStyleReader: function(){
+                var me = this;
+                var oModel = this.getOwnerComponent().getModel();
+                var entitySet = "/StyleStatsSet('')";
+                var oJSONObject = new sap.ui.model.json.JSONModel();
+                var oJSONObject2 = new sap.ui.model.json.JSONModel();
+                var oJSONObject3 = new sap.ui.model.json.JSONModel();
+                var oForecast = this.getView().byId("forecastNumber");
+                var oOrder = this.getView().byId("orderNumber");
+                var oShipped = this.getView().byId("shippedNumber");
+
+                oModel.read(entitySet,{
+                    success:function(oData){
+                        oJSONObject.setData(oData)
+                        oForecast.setText(String(oData.Forecast));
+                    },
+
+                    error: function(err){}
+                });
+
+                oModel.read(entitySet,{
+                    success:function(oData){
+                        oJSONObject.setData(oData)
+                        oOrder.setText(String(oData.Order));
+                    },
+
+                    error: function(err){}
+                });
+
+                oModel.read(entitySet,{
+                    success:function(oData){
+                        oJSONObject.setData(oData)
+                        oShipped.setText(String(oData.Shipped));
+                    },
+
+                    error: function(err){}
+                });
+            }
+        })
     });
