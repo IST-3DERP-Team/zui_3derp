f5b5e2aac2599a388b3da971ff748e300d9194b4
Romeo Antonio Azurin <anton.azurin@bnext.tech>
2022-07-04 07:36:43 +0000

    Added: AssignMaterial page

---
+++ webapp/controller/AssignMaterial.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/library",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, JSONModel) {
+        "use strict";
+
+        return Controller.extend("zui3derp.controller.AssignMaterial", {    
+            onInit: function(){
+                
+            }
+        })
+    })
